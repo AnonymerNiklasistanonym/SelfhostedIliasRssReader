a79a2085b5fa857d5b82b370470075a9e7677f32
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2017-10-22 17:21:20 +0200

    Added some comments and made script a bit faster

--- script.py
+++ script.py
@@ -13,17 +13,10 @@ import sys
 import feedparser
 import requests
 
-
-
 # Setup the Gmail API - set USE_GMAIL False if you want to use the Simplified Gmail API
 USE_GMAIL = True
 
-if USE_GMAIL:
-    # Gmail Imports (not important for the actual crawler)
-    from SendGmailSimplified.SendGmailSimplified import SimplifiedGmailApi
-    from premailer import transform
-
-# Paths for important directories and files - from home directory
+# Paths for important directories and files - from home directory (Cron Job Optimization)
 HOME_DIR = os.path.expanduser('~')
 
 # change this to the directory your script is: !!!!!!!!!!!!!!!!!
@@ -31,24 +24,32 @@ DIR_OF_SCRIPT = os.path.join(HOME_DIR,
                              os.path.join("Documents", "SelfhostedIliasRssReader"))
 # GitHubBeta/SelfhostedIliasRssReader
 
+# Paths to important files
 PATH_FOR_LOG = os.path.join(DIR_OF_SCRIPT, "script.log")
 PATH_OF_DATA_FILE = os.path.join(DIR_OF_SCRIPT, 'data.json')
 PATH_OF_CREDENTIALS_FILE = os.path.join(DIR_OF_SCRIPT, 'credentials.json')
 PATH_OF_CSS_FILE = os.path.join(DIR_OF_SCRIPT, 'main.css')
+PATH_OF_HTML_CHECK_FILE = os.path.join(DIR_OF_SCRIPT, 'check.html')
 
 logging.basicConfig(filename=PATH_FOR_LOG, level=logging.DEBUG)
 
+# read from credentials personal URL and credentials for RSS feed
 if os.path.isfile(PATH_OF_CREDENTIALS_FILE):
+
     with open(PATH_OF_CREDENTIALS_FILE) as json_file:
         DATA = json.load(json_file)
 
+        # get information from file
         URL = DATA["url"]
         USER_NAME = DATA["username"]
         PASSWORD = DATA["password"]
 
+        # read recipients if email is activated
         if USE_GMAIL:
             RECIPIENTS = DATA["recipients"]
+
 else:
+    # if nothing was found quit the script
     print("No credentials and url was found")
     sys.exit()
 
@@ -58,22 +59,26 @@ CONTENT = requests.get(URL, auth=(USER_NAME, PASSWORD))
 # Parse RSS content
 PARSED_CONTENT = feedparser.parse(CONTENT.content)
 
+# Check if the script ever run
 if os.path.isfile(PATH_OF_DATA_FILE):
     with open(PATH_OF_DATA_FILE) as json_file:
         DATA = json.load(json_file)
 
+        # get the saved date from this file
         LATEST_DATE = DATA['date']
 
         print("Date of last update: " + DATA['date'])
         print("Current date of newest entry: " +
               PARSED_CONTENT['entries'][0]['published'])
 
+        # fast check if there are any new entries - quit if yes
         if DATA['date'] == PARSED_CONTENT['entries'][0]['published']:
             print("no new articles")
             sys.exit()
 else:
-    LATEST_DATE = "no date"
+    LATEST_DATE = "no latest date"
 
+# Save the published date of the latest entry in a file
 with open(PATH_OF_DATA_FILE, 'w') as outfile:
     json.dump({"date": PARSED_CONTENT['entries'][0]['published']}, outfile)
 
@@ -81,6 +86,7 @@ print("latest date: " + LATEST_DATE)
 
 WALKING_RSS_STRING = "<h1>New Ilias RSS Entries:</h1>"
 
+# Read all entries and convert them to html
 for entry in PARSED_CONTENT['entries']:
 
     print("current date: " + entry['published'])
@@ -89,7 +95,7 @@ for entry in PARSED_CONTENT['entries']:
         break
     WALKING_RSS_STRING += '<div class="entry"><div class="size"><p class="title">' + \
         entry['title'] + '</p><p class="date">' + \
-        entry['published'] + "</p></div>"
+        entry['published'][:-6] + "</p></div>"
     WALKING_RSS_STRING += '<p class="content">' + \
         entry['summary_detail']['value'] + '</p>'
     WALKING_RSS_STRING += '<a href="' + \
@@ -98,7 +104,12 @@ for entry in PARSED_CONTENT['entries']:
 
 print(WALKING_RSS_STRING)
 
+# Send email with new RSS entries if desired
 if USE_GMAIL:
+    # Gmail Imports (not important for the actual crawler)
+    from SendGmailSimplified.SendGmailSimplified import SimplifiedGmailApi
+    from premailer import transform
+
     with open(PATH_OF_CSS_FILE, 'r') as cssfile:
         CSS_DATA = cssfile.read().rstrip("\n")
 
@@ -106,6 +117,9 @@ if USE_GMAIL:
                   "</style></head><body>" + WALKING_RSS_STRING + "</body></html>")
 
     EMAIL_TEXT = transform(EMAIL_TEXT).replace("\n", "")
+
+    with open(PATH_OF_HTML_CHECK_FILE, 'w') as outfile:
+        outfile.write(EMAIL_TEXT)
 
     DIR_OF_GMAIL_API_FILES = os.path.join(DIR_OF_SCRIPT,
                                           os.path.join("SendGmailSimplified",
@@ -121,15 +135,3 @@ if USE_GMAIL:
     for recipient in RECIPIENTS:
         GMAIL_SERVER.send_html(
             recipient, "Ilias RSS Update", EMAIL_TEXT)
-
-
-if USE_GMAIL:
-    with open("aha.html", 'w') as outfile:
-        with open(PATH_OF_CSS_FILE, 'r') as cssfile:
-            CSS_DATA = cssfile.read().rstrip("\n")
-
-        EMAIL_TEXT = ('<!DOCTYPE html><html><head><style type="text/css">' + CSS_DATA +
-                    "</style></head><body>" + WALKING_RSS_STRING + "</body></html>")
-
-        EMAIL_TEXT = transform(EMAIL_TEXT).replace("\n", "")
-        outfile.write(EMAIL_TEXT)
